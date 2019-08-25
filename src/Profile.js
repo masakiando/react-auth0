b97cde314dfc80b965a747a6fa375f86512504b8
Your Name <you@example.com>
2019-08-25 18:01:09 +0900

    Newプロシェクト生成,不要なファイルコード削除

---
+++ src/Profile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+    render() {
+        return (
+            <div>
+               <h1>Profile</h1>
+               <div>hello</div> 
+            </div>
+        );
+    }
+}
+
+export default Profile;
